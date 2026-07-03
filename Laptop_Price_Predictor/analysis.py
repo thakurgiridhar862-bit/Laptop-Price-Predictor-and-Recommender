be372eaeb9706b7729b58b7be27960a90826dc7c
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-03 20:41:33 +0530

    Add brand analysis

--- Laptop_Price_Predictor/analysis.py
+++ Laptop_Price_Predictor/analysis.py
@@ -88,11 +88,75 @@ def price_dist(df):
     plt.close()
 
 
+def brand_analysis(df):
+
+    print("\nBRAND ANALYSIS")
+    print("-" * 50)
+
+    print(f"Total Brands : {df['Brand'].nunique()}")
+    print("Brand counts")
+    print("-" * 50)
+    br_count = df["Brand"].value_counts()
+    print(br_count)
+    print(f"Most Common Brand : {br_count.index[0]} ({br_count.iloc[0]} laptops)")
+
+    # graph 1
+    plt.figure(figsize=(16, 5))
+    plt.title("Top 10 Laptop Brands")
+    plt.xlabel("Brands")
+    plt.ylabel("Number of laptops")
+    plt.grid(axis="y", alpha=0.3)
+    X = br_count.head(10).index
+    Y = br_count.head(10).values
+    ax1 = sns.barplot(
+        x=X,
+        y=Y,
+        palette="crest",
+    )
+    for i in ax1.containers:
+        ax1.bar_label(i, padding=5)
+    plt.savefig(
+        "Laptop_Price_Predictor/graphs/top10_brands.png", dpi=300, bbox_inches="tight"
+    )
+    plt.show()
+    plt.close()
+
+    avg_brand_price = (
+        df.groupby("Brand")["Price_Euro"].mean().round(2).sort_values(ascending=False)
+    )
+    print("Average Price by brand")
+    print("-" * 50)
+    print(avg_brand_price)
+    # graph 2
+    plt.figure(figsize=(16, 5))
+    plt.title("Average Laptop Price by Brand")
+    plt.xlabel("Brand")
+    plt.ylabel("Average Price (Euro)")
+    plt.grid(axis="y", alpha=0.3)
+    X = avg_brand_price.head(10).index
+    Y = avg_brand_price.head(10).values
+    ax2 = sns.barplot(
+        x=X,
+        y=Y,
+        palette="rocket",
+    )
+    for i in ax2.containers:
+        ax2.bar_label(i, padding=5)
+    plt.savefig(
+        "Laptop_Price_Predictor/graphs/average_price_by_brand.png",
+        dpi=300,
+        bbox_inches="tight",
+    )
+    plt.show()
+    plt.close()
+
+
 def main():
     df = load_data()
     data_overview(df)
     df = clean_data(df)
     price_dist(df)
+    brand_analysis(df)
 
 
 if __name__ == "__main__":
