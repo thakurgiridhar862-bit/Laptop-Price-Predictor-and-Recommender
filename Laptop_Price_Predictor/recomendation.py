bace4c60dad02553a9ceaf6d24d68584339f05a4
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-04 12:56:48 +0530

    Added RAM and storage recommendation filters

--- Laptop_Price_Predictor/recomendation.py
+++ Laptop_Price_Predictor/recomendation.py
@@ -12,6 +12,22 @@ def currency_conversion(df):
     return df
 
 
+def user_input(df):
+    budget = int(input("Enter Your Budget (INR) : "))
+    ram = int(input("Enter Minimum RAM (GB) : "))
+    storage = int(input("Enter Minimum Storage (GB) : "))
+
+    print("\nAvailable Brands")
+    print("-" * 50)
+    print(df["Brand"].unique())
+
+    brand = input("\nEnter Preferred Brand : ")
+
+    touch = input("\nTouch Screen (Yes/No) : ")
+
+    return budget, ram, storage, brand, touch
+
+
 def budget_filter(df, budget):
     fil_df = df[df["Price_INR"] <= budget]
 
@@ -34,11 +50,24 @@ def ram_filter(rec_df, ram):
     return fil_df
 
 
+def storage_filter(rec_df, storage):
+    fil_df = rec_df[rec_df["Storage"] >= storage]
+
+    print("\nStorage Filter Applied")
+    print("-" * 50)
+    print(f"Preffered Storage : {storage} GB")
+    print(f"Matching Laptops  : {fil_df.shape[0]}")
+
+    return fil_df
+
+
 def main():
     df = load_data()
     df = currency_conversion(df)
+    budget, ram, storage, brand, touch = user_input(df)
     rec_df = budget_filter(df, budget)
-    rec_df = ram_filter(df, ram)
+    rec_df = ram_filter(rec_df, ram)
+    rec_df = storage_filter(rec_df, storage)
 
 
 if __name__ == "__main__":
