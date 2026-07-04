6ede1a1828df5e257d3b54103442fd0c47ecb347
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-04 12:44:32 +0530

    Added ram and budget filter

--- Laptop_Price_Predictor/recomendation.py
+++ Laptop_Price_Predictor/recomendation.py
@@ -12,9 +12,33 @@ def currency_conversion(df):
     return df
 
 
+def budget_filter(df, budget):
+    fil_df = df[df["Price_INR"] <= budget]
+
+    print("\nBudget Filter Applied")
+    print("-" * 50)
+    print(f"Budget : ₹{budget}")
+    print(f"Matching Laptops : {fil_df.shape[0]}")
+
+    return fil_df
+
+
+def ram_filter(rec_df, ram):
+    fil_df = rec_df[rec_df["RAM"] >= ram]
+
+    print("\nRAM Filter Applied")
+    print("-" * 50)
+    print(f"Preffered RAM : {ram} GB")
+    print(f"Matching Laptops : {fil_df.shape[0]}")
+
+    return fil_df
+
+
 def main():
     df = load_data()
     df = currency_conversion(df)
+    rec_df = budget_filter(df, budget)
+    rec_df = ram_filter(df, ram)
 
 
 if __name__ == "__main__":
