54193611fbcc51d411b416b64231cd75a91db95e
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-04 13:09:33 +0530

    Implemented laptop recommendation engine with filtering

--- Laptop_Price_Predictor/recomendation.py
+++ Laptop_Price_Predictor/recomendation.py
@@ -66,20 +66,72 @@ def brand_filter(rec_df, brand):
 
     print("\nBrand Filter Applied")
     print("-" * 50)
-    print(f"Preffered Brand : {brand} GB")
+    print(f"Preffered Brand : {brand}")
     print(f"Matching Laptops  : {fil_df.shape[0]}")
 
     return fil_df
 
 
+def touch_filter(rec_df, touch):
+    fil_df = rec_df[rec_df["Touch"] == touch]
+
+    print("\nTouch Filter Applied")
+    print("-" * 50)
+    print(f"Is touch screen ? : {touch}")
+    print(f"Matching Laptops  : {fil_df.shape[0]}")
+
+    return fil_df
+
+
+def top_rec(rec_df):
+    rec_df = rec_df.sort_values(
+        by=["Price_INR", "RAM", "Storage"],
+        ascending=[True, False, False],
+    )
+
+    rec_df = rec_df.head(5)
+
+    print("\nTop 5 Recommended Laptops")
+    print("-" * 50)
+
+    print(
+        rec_df[
+            [
+                "Laptop",
+                "Brand",
+                "CPU",
+                "RAM",
+                "Storage",
+                "Storage_Type",
+                "GPU",
+                "Screen",
+                "Touch",
+                "Price_Euro",
+                "Price_INR",
+            ]
+        ]
+    )
+
+    return rec_df
+
+
 def main():
     df = load_data()
     df = currency_conversion(df)
+
     budget, ram, storage, brand, touch = user_input(df)
+
     rec_df = budget_filter(df, budget)
     rec_df = ram_filter(rec_df, ram)
     rec_df = storage_filter(rec_df, storage)
     rec_df = brand_filter(rec_df, brand)
+    rec_df = touch_filter(rec_df, touch)
+
+    if len(rec_df) == 0:
+        print("\nNo Laptop Found")
+        print("Try increasing your budget or changing your requirements.")
+    else:
+        rec_df = top_rec(rec_df)
 
 
 if __name__ == "__main__":
